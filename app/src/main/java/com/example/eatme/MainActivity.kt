b69e00c44c3693fd865ac9a10059492401722b4a
feketeszili <32087212+feketeszili@users.noreply.github.com>
2020-12-01 11:31:54 +0200

    Fragments Created

--- app/src/main/java/com/example/eatme/MainActivity.kt
+++ app/src/main/java/com/example/eatme/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.eatme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.eatme.userInterface.DetailsFragment
+import com.example.eatme.userInterface.ProfileFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun changeFragment(view: View){
+        var fragment: Fragment
+
+        if(view == findViewById(R.id.button)){
+            fragment = DetailsFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_place, fragment).commit()
+        }
+        if( view == findViewById(R.id.button2)){
+            fragment = ProfileFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_place, fragment).commit()
+        }
+
+    }
+
+
 }
