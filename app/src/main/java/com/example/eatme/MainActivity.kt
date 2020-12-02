cdc22a988cb5c80e86587ae3ebdba7735d64097c
feketeszili <32087212+feketeszili@users.noreply.github.com>
2020-12-02 10:15:13 +0200

    Fragments Created

--- app/src/main/java/com/example/eatme/MainActivity.kt
+++ app/src/main/java/com/example/eatme/MainActivity.kt
@@ -6,19 +6,21 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.eatme.userInterface.DetailsFragment
 import com.example.eatme.userInterface.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigationView: BottomNavigationView = findViewById(R.id.navigation_view)
 
     }
 
     fun changeFragment(view: View){
         var fragment: Fragment
 
-        if(view == findViewById(R.id.button)){
+        /*if(view == findViewById(R.id.button)){
             fragment = DetailsFragment()
             supportFragmentManager.beginTransaction().replace(R.id.fragment_place, fragment).commit()
         }
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             fragment = ProfileFragment()
             supportFragmentManager.beginTransaction().replace(R.id.fragment_place, fragment).commit()
         }
-
+*/
     }
 
 
