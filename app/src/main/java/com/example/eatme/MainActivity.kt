7a31f7c5fc0c5e13febab76618e59106c995d2d2
feketeszili <32087212+feketeszili@users.noreply.github.com>
2020-12-06 15:15:00 +0200

    Navigation is working

--- app/src/main/java/com/example/eatme/MainActivity.kt
+++ app/src/main/java/com/example/eatme/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.eatme.userInterface.DetailsFragment
+import com.example.eatme.userInterface.FavouriteFragment
 import com.example.eatme.userInterface.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,14 +14,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val detailsFragment= DetailsFragment()
+        val favouriteFragment = FavouriteFragment()
+        val profileFragment = ProfileFragment()
         val navigationView: BottomNavigationView = findViewById(R.id.navigation_view)
+
+        navigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.nav_add ->makeCurrentFragment(detailsFragment)
+                R.id.nav_favourite -> makeCurrentFragment(favouriteFragment)
+                R.id.nav_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
 
     }
 
-    fun changeFragment(view: View){
+    fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply{
+          replace(R.id.fragment_container_view_tag, fragment)
+            commit()
+    }
+
+    /*fun changeFragment(view: View){
         var fragment: Fragment
 
-        /*if(view == findViewById(R.id.button)){
+        if(view == findViewById(R.id.button)){
             fragment = DetailsFragment()
             supportFragmentManager.beginTransaction().replace(R.id.fragment_place, fragment).commit()
         }
